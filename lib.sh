7e15b1288b0e6dc239faa73c7ced2d893b60bc6a
Renat Gabdulhakov <rgabdulhakov@natera.com>
2017-10-29 23:41:50 -0700

    Use parallel instead of forking

--- lib.sh
+++ lib.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
-user_email='rgabdulhakov@natera.com'
-user_key='mV0DNRm3RFyFqd2lIvNp-z5nl5ZLIrC.B0LKyGy0z'
-url='https://testrail.natera.com'
+export user_email='rgabdulhakov@natera.com'
+export user_key='mV0DNRm3RFyFqd2lIvNp-z5nl5ZLIrC.B0LKyGy0z'
+export url='https://testrail.natera.com'
+export treads=16
 
 debug() {
   >&2 echo -e "\033[0;32m$@\033[0m"
 }
+export -f debug
 
 warning() {
   >&2 echo -e "\033[1;33m$@\033[0m"
@@ -14,17 +16,19 @@ warning() {
 
 error() {
   >&2 echo -e "\033[0;31m$@\033[0m"
-  exit 1
+  return 1
 }
 
 api_request() {
-  curl -fs -H "Content-Type: application/json" -u "${user_email}:${user_key}" "$@"
+  curl -fs -H "Content-Type: application/json" -u "${user_email:?User is empty}:${user_key:?Password or key is empty}" "$@"
 }
+export -f api_request
 
 get_case() {
   local id=${1:?Case ID is required}
   api_request "${url}/index.php?/api/v2/get_case/${id}" || error "Couldn't get case with ID $id"
 }
+export -f get_case
 
 get_cases_by_section() {
   local project=${1:?Project ID is required}
@@ -32,12 +36,34 @@ get_cases_by_section() {
   local id=${3:?Section ID is required}
   api_request "${url}/index.php?/api/v2/get_cases/${project}&suite_id=${suite}&section_id=${id}" || error "Couldn't get cases by section ID $id"
 }
+export -f get_cases_by_section
+
+get_section() {
+  local section_id=${1:?Section ID is required}
+  api_request "${url}/index.php?/api/v2/get_section/${section_id}" || error "Couldn't get section $section"
+}
+export -f get_section
+
+get_sections() {
+  local project=${1:?Project ID is required}
+  local suite=${2:?Suite ID is required}
+  api_request "${url}/index.php?/api/v2/get_sections/${project}&suite_id=${suite}" || error "Couldn't get sections for $project project and $suite suite"
+}
+export -f get_sections
 
 update_case() {
   local id=${1:?Case ID is required}
   local body=${2:?Test body is required}
   api_request "${url}/index.php?/api/v2/update_case/${id}" -X POST -d "$body" || error "Couln't update case with ID $id"
 }
+export -f update_case
+
+update_section() {
+  local id=${1:?Section ID is required}
+  local body=${2:?Section body is required}
+  api_request "${url}/index.php?/api/v2/update_section/${id}" -X POST -d "$body" || error "Couln't update case with ID $id"
+}
+export -f update_section
 
 edit_case() {
   local id=${1:?Case ID is required}
@@ -50,49 +76,27 @@ edit_case() {
     error "Failed to edit case $id with $regexp"
   fi
 }
+export -f edit_case
 
 edit_cases() {
   local cases_id="${1:?Cases ID are required}"
   local regexp="${2:?Regexp is required}"
-  for id in $cases_id
-  do
-    edit_case $id $"$regexp" &
-  done
-  wait
+  tr ' ' '\n' <<< $cases_id | parallel -q -j$treads edit_case {} $"$regexp"
 }
-
-get_section() {
-  local section_id=${1:?Section ID is required}
-  api_request "${url}/index.php?/api/v2/get_section/${section_id}" || error "Couldn't get section $section"
-}
-
-get_sections() {
-  local project=${1:?Project ID is required}
-  local suite=${2:?Suite ID is required}
-  api_request "${url}/index.php?/api/v2/get_sections/${project}&suite_id=${suite}" || error "Couldn't get sections for $project project and $suite suite"
-}
-
-update_section() {
-  local id=${1:?Section ID is required}
-  local body=${2:?Section body is required}
-  api_request "${url}/index.php?/api/v2/update_section/${id}" -X POST -d "$body" || error "Couln't update case with ID $id"
-}
+export -f edit_cases
 
 edit_section() {
   local id=${1:?Section ID is required}
-  local regexp="${2:? RegExp is required}"
-  local body=$(sed "$regexp" <(get_case $id))
-  update_section $id "$body" | jq -r '.id'
+  local regexp="${2:?RegExp is required}"
+  local body=$(sed "$regexp" <(get_section $id))
+  update_section $id "${body:?Body is empty}" | jq -r '.id'
 }
+export -f edit_section
 
 edit_sections() {
   local sections_id="${1:?Sections ID are required}"
   local regexp="${2:?Regexp is required}"
-  for id in $sections_id
-  do
-    edit_section $id "$regexp" &
-  done
-  wait
+  tr ' ' '\n' <<< $sections_id | parallel -q -j$treads edit_section {} $"$regexp"
 }
 
 get_nested_sections () {
@@ -103,11 +107,11 @@ get_nested_sections () {
   then
     for root in $root_sections_ids
     do
-      childron="$(echo "$sections" | jq -M ".[] | select(.parent_id == $root) | .id")"
-      if [ -n "$childron" ]
+      children="$(echo "$sections" | jq -M ".[] | select(.parent_id == $root) | .id")"
+      if [ -n "$children" ]
       then
-        echo $childron
-        get_nested_sections "$sections" "$childron" &
+        echo $children
+        get_nested_sections "$sections" "$children" &
       fi
     done
 
@@ -149,11 +153,7 @@ get_nested_cases_by_section_name() {
   local suite=${2:?Suite ID is required}
   local section_name="${3:?Section name is required}"
   local nested_sections=$(get_nested_sections_by_name $project $suite "$section_name")
-  for nested_section_id in $nested_sections
-  do
-    get_cases_by_section $project $suite $nested_section_id | jq -M '.[] | .id' &
-  done
-  wait
+  tr ' ' '\n' <<< $nested_sections | parallel -q -j$treads get_cases_by_section $project $suite {} |  jq -M '.[] | .id'
 }
 
 get_nested_cases_by_section_id() {
@@ -161,11 +161,7 @@ get_nested_cases_by_section_id() {
   local suite=${2:?Suite ID is required}
   local section_id=${3:?Section ID is required}
   local nested_sections=$(get_nested_sections_by_id $project $suite $section_id)
-  for nested_section_id in $nested_sections
-  do
-    get_cases_by_section $project $suite $nested_section_id | jq -M '.[] | .id' &
-  done
-  wait
+  tr ' ' '\n' <<< $nested_sections | parallel -q -j$treads get_cases_by_section $project $suite {} | jq -M '.[] | .id'
 }
 
 _test() {
